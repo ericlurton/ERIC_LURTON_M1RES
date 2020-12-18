9304b3bd24ecc0a57f3e2ec77bf07a3b9ff84b8f
ericlurton <75683909+ericlurton@users.noreply.github.com>
2020-12-18 12:00:08 +0100

    Add files via upload

---
+++ TD4.py
@@ -0,0 +1,166 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+#Exercice 1
+
+#1
+def surf_circle(*args): #surf_circle définis en *args pour avoir accès a Pi
+#On importe pi de la fonction math
+    from math import pi
+#On créé une variable "surface" avec le calcul de la surface du cercle en fonction de son rayon qu'on a définis plus haut
+    surface = pi * (args[0] ** 2)
+#return == va retourner & concrétement print le résultat
+    return surface
+
+print(surf_circle(2.5))
+
+#2
+#Modification du prgrm pour faire ne sorte que l'argument est par défault la valeur 1
+def surf_circle_2(arg_1=1):
+    from math import pi
+    surface = pi * (arg_1 ** 2)
+    return surface
+
+print(surf_circle_2(8))
+
+#3
+def volboite(x1,x2,x3)
+    calcul=(x1*x2*x3)
+    return calcul
+
+print volboite(1,3,9)
+
+#4
+def volboite(x1,x2,x3)
+    calcul=(x1*x2*x3)
+    return calcul
+
+print volboite(1,3,9)
+
+#5
+def remplacement(arg_1=3)
+    arg_1 = c1
+    arg_2 = c2
+    arg_3 = ch
+    c1=" "
+    c2="*"
+    ch = "Je fais un test juste pour voir en théorie, les espaces devraient être remplacé par des étoiles grâce à la fonction replace"
+
+print ch.replace(c1,c2)
+
+#6
+def remplacement(*args)
+    ch2 = "Test d'une autre manière"
+    print(ch2.replace(" ","*"))
+
+
+# In[14]:
+
+
+#Exo 2
+
+def nb_char(chaine):
+    counter = 0 #Création d'une variable counter à 0
+    for c in chaine: #Par caractère dans le texte, counter gagne 1
+        counter += 1
+    print(counter) #On affiche le nombre de caractère compté
+
+print(nb_char(chaine="Décret n° 2020-1600 du 16 décembre 2020 relatif à la composition, à l'organisation et au fonctionnement du conseil de surveillance prévu à l'article L. 253-8 du code rural et de la pêche maritime"))
+
+
+def nb_mots(chaine):
+    mots = len(chaine.split())
+    return mots
+
+print(nb_mots(chaine="Décret n° 2020-1600 du 16 décembre 2020 relatif à la composition, à l'organisation et au fonctionnement du conseil de surveillance prévu à l'article L. 253-8 du code rural et de la pêche maritime"))
+
+
+# In[22]:
+
+
+#Exo 3
+
+def multi(n, debut=1, fin=10):
+    i=debut
+    while i!=fin:
+        print(n*i)
+        i=i+1
+
+multi(6)
+
+def multi2(n, debut=1, fin=10):
+    liste = []
+    if debut > fin:
+        for i in range(fin, debut + 1):
+            liste.append(n * i)
+        liste.reverse()
+        print(liste)
+    else:
+        for i in range(debut, fin + 1):
+            liste.append(n * i)
+        print(liste)
+        
+def multi3():
+    ndébutfin=[]
+    n = int(input("Entrer n"))
+    début = int(input("Entrer le début"))
+    fin = int(input("Entrer la fin"))
+    ndébutfin.append(n,début,fin)
+    
+    liste = []
+    if debut > fin:
+        for i in range(fin, debut + 1):
+            liste.append(n * i)
+        liste.reverse()
+        print(liste)
+    else:
+        for i in range(debut, fin + 1):
+            liste.append(n * i)
+        print(liste)
+
+
+# In[ ]:
+
+
+def motus(arg_1=10):
+    from random import randint
+    mots = ["arbre", "grave", "piece", "nuage", "crane", "sonne", "table", "herbe", "ecrou", "mulet"]
+    number = randint(0, len(mots) - 1)
+    random_word = mots[number]
+    word = []
+    word[:0] = random_word
+    user = []
+    for x in range(len(word)):
+        user.append("-")
+    print(" ".join(user))
+    compteur = 0
+
+    while True:
+        compteur += 1
+        if compteur == arg_1 + 1:
+            break
+        else:
+            print("Essai numéro : " + str(compteur))
+            ask = input("Entrer une lettre : ")
+            for i in range(len(word)):
+                if ask == word[i]:
+                    user[i] = word[i]
+                    if user == word:
+                        print(" ".join(user))
+                        win = input("Bravo! Rejouer ? y/n ")
+                        if win == "y":
+                            motus()
+                        else:
+                            exit()
+                else:
+                    pass
+            print(" ".join(user))
+
+    rejoue = input("Perdu... Rejoue y/n : ")
+    if rejoue == "y":
+        motus()
+#motus()
+
