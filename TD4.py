bff5254a6f52bd1af4bc655b0c2c5d1ed073637f
ericlurton <75683909+ericlurton@users.noreply.github.com>
2020-12-20 15:59:59 +0100

    Add files via upload

--- TD4.py
+++ TD4.py
@@ -7,38 +7,43 @@
 #Exercice 1
 
 #1
-def surf_circle(*args): #surf_circle définis en *args pour avoir accès a Pi
+def surf_circle(a): #On demande un "argument" une valeur qui prendra "a"
 #On importe pi de la fonction math
     from math import pi
 #On créé une variable "surface" avec le calcul de la surface du cercle en fonction de son rayon qu'on a définis plus haut
-    surface = pi * (args[0] ** 2)
+    surface=pi*(a**2)
 #return == va retourner & concrétement print le résultat
     return surface
 
-print(surf_circle(2.5))
+print(surf_circle(7.5))
 
 #2
 #Modification du prgrm pour faire ne sorte que l'argument est par défault la valeur 1
-def surf_circle_2(arg_1=1):
+def surf_circle_2(b=1):
     from math import pi
-    surface = pi * (arg_1 ** 2)
+    surface=pi*(b**2)
     return surface
 
 print(surf_circle_2(8))
 
 #3
-def volboite(x1,x2,x3)
-    calcul=(x1*x2*x3)
+def volboite(c,d,e):#On demande les 3 côtés de la boite puis calc volume
+    calcul=(c*d*e)
     return calcul
 
-print volboite(1,3,9)
+print(volboite(1,3,9))
 
 #4
-def volboite(x1,x2,x3)
-    calcul=(x1*x2*x3)
-    return calcul
+def volboite(f,g,h):
+    if g==null or h ==null: #Si g ou h non remplis
+        g and h == f #g et h prennent la valeur de f
+        calcul=(f*g*h) #calcul classique
+        return calcul
+    else:
+        calcul=(f*g*h)#autrement
+        return calcul
 
-print volboite(1,3,9)
+print(volboite(1,3,9))
 
 #5
 def remplacement(arg_1=3)
@@ -83,24 +88,24 @@ print(nb_mots(chaine="Décret n° 2020-1600 du 16 décembre 2020 relatif à la c
 
 #Exo 3
 
-def multi(n, debut=1, fin=10):
+def multi(n, debut=1, fin=10): #On définis les 3 arguments début, fin et une variable n
     i=debut
     while i!=fin:
-        print(n*i)
+        print(n*i) #Afficher les multiplications de n commençant a "début=1" et finissant à fin
         i=i+1
 
 multi(6)
 
 def multi2(n, debut=1, fin=10):
-    liste = []
-    if debut > fin:
-        for i in range(fin, debut + 1):
-            liste.append(n * i)
+    liste=[]
+    if debut>fin:
+        for i in range(fin, debut+1):
+            liste.append(n*i)
         liste.reverse()
         print(liste)
     else:
-        for i in range(debut, fin + 1):
-            liste.append(n * i)
+        for i in range(debut,fin+1):
+            liste.append(n*i)
         print(liste)
         
 def multi3():
@@ -125,14 +130,14 @@ def multi3():
 # In[ ]:
 
 
-def motus(arg_1=10):
-    from random import randint
+def motus(arg_1=10): #10 entrée possible
+    from random import randint #On importe la fonction randint
     mots = ["arbre", "grave", "piece", "nuage", "crane", "sonne", "table", "herbe", "ecrou", "mulet"]
-    number = randint(0, len(mots) - 1)
-    random_word = mots[number]
-    word = []
-    word[:0] = random_word
-    user = []
+    choixmot=mots(randint(0,9)) #0-9 car la liste mots commence à 0
+    motalpha=[] #On créé une liste dans laquel on récupérera le mot choisis aléatoirement
+    motalpha[0]=choixmot #En caractère car dans une liste
+    
+#############################################################################################################################
     for x in range(len(word)):
         user.append("-")
     print(" ".join(user))
@@ -163,4 +168,10 @@ def motus(arg_1=10):
     if rejoue == "y":
         motus()
 #motus()
+
+
+# In[ ]:
+
+
+
 
